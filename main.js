b59e79bd0096f1dd8ca4f4d13265107409f3e270
Joel-Blanco <joel.blanco53@outlook.com>
2024-05-13 19:18:32 +0200

    done.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const name = prompt("Please enter your first name");
 const lastName = prompt("Please enter your lastname");
 const favColor = prompt("What's your favorite color?");
 
-const fullPwd = `${name}${lastname}${favcolor}${"21"}`;
-document.getElementById("pwd").innerHTML = fullpwd;
-console.log(fullpwd);
+const fullPwd = `${name}${lastName}${favColor}${"21"}`;
+document.getElementById("pwd").innerHTML = fullPwd;
+console.log(fullPwd);
 
 // console only without ${"21"}
 //console.log(fullpwd + 21); 
